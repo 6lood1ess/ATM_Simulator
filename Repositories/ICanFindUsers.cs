6dd58ee83ced17d1436265e4597b27727d56733f
6lood1ess <6lo.od1ess@gmail.com>
2026-05-28 20:50:10 +0700

    Added: - The ICanFindUsers interface, which defines a contract for working with the data store. It declares four methods: searching for a user by card number, searching for a card + PIN pair, getting a list of all users, and updating the balance of a specified user; - The UserRepository class, which uses a built-in in-memory user list. Upon initialization, it creates three test users (Cristiano Ronaldo, Nikita Zakharov, and Bruce Wayne) with preset balances. It provides LINQ search and thread-safe balance updating by card number.

---
+++ Repositories/ICanFindUsers.cs
@@ -0,0 +1,11 @@
+﻿using ATM_Simulator.Models;
+using System.Collections.Generic;
+
+namespace ATM_Simulator.Repositories {
+  public interface ICanFindUsers {
+    User FindByCardNumber(string cardNumber);
+    User FindByCredentials(string cardNumber, string pinCode);
+    List<User> GetAllUsers();
+    void UpdateUserBalance(string cardNumber, double newBalance);
+  }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using ATM_Simulator.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ATM_Simulator.Repositories {
+  public class UserRepository : ICanFindUsers {
+    private readonly List<User> _users;
+
+    public UserRepository() {
+      _users = new List<User> {
+        new User("Криштиано Роналдо", "8584739463025264", "6767", 5),
+        new User("Никита Захаров", "5837274969570736", "1915", 4064),
+        new User("Брюс Уэйн", "3958729600658263", "0707", 1039458)
+      };
+    }
+
+    public User FindByCardNumber(string cardNumber) {
+      return _users.FirstOrDefault(user => user.CardNumber == cardNumber);
+    }
+
+    public User FindByCredentials(string cardNumber, string pinCode) {
+      return _users.FirstOrDefault(user => user.CardNumber == cardNumber && user.PinCode == pinCode);
+    }
+
+    public List<User> GetAllUsers() {
+      return _users.ToList();
+    }
+
+    public void UpdateUserBalance(string cardNumber, double newBalance) {
+      User user = FindByCardNumber(cardNumber);
+
+      if (user != null) {
+        user.Balance = newBalance;
+      }
+    }
+  }
+}
