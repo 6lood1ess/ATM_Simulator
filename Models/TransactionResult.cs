2fa1f00c369e7a93b3d4c4fcd75e9c892212879a
6lood1ess <6lo.od1ess@gmail.com>
2026-05-28 20:14:49 +0700

    Added: - User class, representing a banking system user. Contains four fields: Name, CardNumber, PinCode, and Balance. The class constructor initializes all fields when creating a new user instance; - TransactionResult class for unified return of banking transaction results. Encapsulates three fields: a success flag (Success), a text message for the user (Message), and an updated balance (NewBalance), which may be null in the event of an error or invalid transaction.

---
+++ Models/TransactionResult.cs
@@ -0,0 +1,13 @@
+﻿namespace ATM_Simulator.Models {
+  public class TransactionResult {
+    public bool Success;
+    public string Message;
+    public double? NewBalance;
+
+    public TransactionResult(bool success, string message, double? newBalance = null) {
+      Success = success;
+      Message = message;
+      NewBalance = newBalance;
+    }
+  }
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace ATM_Simulator.Models {
+  public class User {
+    public string Name;
+    public string CardNumber;
+    public string PinCode;
+    public double Balance;
+
+    public User(string name, string cardNumber, string pinCode, double balance) {
+      Name = name;
+      CardNumber = cardNumber;
+      PinCode = pinCode;
+      Balance = balance;
+    }
+  }
+}
